7a996e2c5bf127a7c882445691cd374e09f46862
Alshafaee Ahmed <alshafaeeahmed@gmail.com>
2022-08-22 14:04:16 +0300

    make Sale page

--- src/components/Change.js
+++ src/components/Change.js
@@ -1,10 +1,36 @@
-import React from "react";
+/*----------------------------------------------------------------------------
+|
+| Module Details: Change
+|
+| Name: Change
+|
+| Purpose: 
+|
+| Date      	Name Ver. Comments
+| --------- ------- ----- ----- -----------------------------------------
+| 22-August-22 	Daher $$1 Created  
+----------------------------------------------------------------------------*/
+import React from 'react';
+import { useState } from "react"
+import { Sele } from './Sele';
+import { Buy } from './Buy';
 
-function Change() {
+export function Change() {
+    const [modallsOpen, SetModallsOpen] = useState(false);
+
+    function ChangeSele() {
+        SetModallsOpen(true);
+    }
+    function ChangeBuy() {
+        SetModallsOpen(false);
+    }
+
     return (
         <div>
-            <button className='Sale' oncancel={}
+            <button className='Sele' onClick={ChangeSele}>Sele</button>
+            <button className='Buy' onClick={ChangeBuy}>buy</button>
+            {modallsOpen ? <Sele /> : <Buy />}
         </div>
-
     );
 }
+export default Change;
